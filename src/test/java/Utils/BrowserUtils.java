ab911f705764b073ba2f05f42fe8415583b4ebd8
Ozangit config --global user.email ozancifcisofteng@gmail.com <ozancifcisofteng@gmail.com>
2023-05-27 15:48:47 -0400

    test2 added

---
+++ src/test/java/Utils/BrowserUtils.java
@@ -0,0 +1,33 @@
+package Utils;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowserUtils {
+
+    public static String getText(WebElement element){
+        return element.getText().trim();
+    }
+
+    public static void selectBy(WebElement location, String value,String methodName){
+
+        Select select=new Select(location);
+
+        switch (methodName){
+            case "text":
+               select.selectByVisibleText(value);
+               break;
+            case "value":
+            select.selectByValue(value);
+            break;
+            case "index":
+                select.selectByIndex(Integer.parseInt(value));
+                break;
+            default:
+                System.out.println("Your methodname is not correct");
+                Assert.assertTrue(false);
+        }
+    }
+}
---
+++ src/test/java/lambda_test/Test2.java
@@ -0,0 +1,2 @@
+package lambda_test;public class Test2 {
+}
